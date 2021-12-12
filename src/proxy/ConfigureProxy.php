c80f9d077063b76233b241b4f9756d1d4ed48635
qingbing <780042175@qq.com>
2021-12-13 05:51:54 +0800

    添加表单配置获取

--- src/proxy/ConfigureProxy.php
+++ src/proxy/ConfigureProxy.php
@@ -8,10 +8,39 @@
 namespace YiiHelper\proxy;
 
 use YiiHelper\proxy\base\InnerProxy;
+use Zf\Helper\DataStore;
 
+/**
+ * 系统代理: 配置系统代理
+ *
+ * Class ConfigureProxy
+ * @package YiiHelper\proxy
+ */
 class ConfigureProxy extends InnerProxy
 {
-    const URL_TEST = '/inner/test/index';
+    const URL_FORM_SETTING = '/inner/public/form-setting';
+    const URL_TEST         = '/inner/test/index';
+
+    /**
+     * 获取配置系统中配置表单信息
+     *
+     * @param string $key
+     * @param string|null $subKey
+     * @param null $default
+     * @return \yii\httpclient\Response
+     */
+    public function formSetting(string $key, ?string $subKey = null, $default = null)
+    {
+        $settings = DataStore::get(__CLASS__ . ':formSetting', function () use ($key) {
+            return $this->send(self::URL_FORM_SETTING, [
+                'key' => $key,
+            ]);
+        });
+        if (empty($subKey)) {
+            return $settings;
+        }
+        return $settings[$subKey] ?? $default;
+    }
 
     public function test()
     {
